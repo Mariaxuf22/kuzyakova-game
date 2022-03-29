c8da6bdb771936f287c26dec46ad531111e2889d
Mariaxuf22 <97722846+Mariaxuf22@users.noreply.github.com>
2022-03-29 11:49:19 +0300

    Add files via upload

---
+++ Matching-Game-main/MatchingGame/MatchingGame/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+    {
+        "!", "!", "N", "N", ",", ",", "k", "k",
+        "b", "b", "v", "v", "w", "w", "z", "z"
+    };
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+            
+                Label clickedLabel = sender as Label;
+
+                if (clickedLabel != null)
+                {
+                    // If the clicked label is black, the player clicked
+                    // an icon that's already been revealed --
+                    // ignore the click
+                    if (clickedLabel.ForeColor == Color.Black)
+                        return;
+
+                    clickedLabel.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
